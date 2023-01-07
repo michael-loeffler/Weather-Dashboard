739772e015d9bfde46f3fa9c96c75de85768ad4e
mloeff <michaelloeffler23@gmail.com>
2023-01-07 17:01:06 -0600

    adds functionality that adds the city you just searched for to a list underneath the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //-- DOM DECLARATIONS --//
 var cityInputEl = $('#citySearch'); // jquery selector for the 
+var cityListEl = $('#cityList'); //jquery selector for the 
 var searchBtn = $('.btn'); // jquery selector for the 
 var todaysForecastEl = $('#todaysForecast'); // jquery selector for the 
 //-- VARIABLE INITIALIZATIONS --//
@@ -7,12 +8,19 @@ var cityInputVal = "";
 var today = dayjs().format('(M/D/YY)');
 var weatherAPI
 //-- EVENT LISTENTERS --//
-searchBtn.on("click", getCityName);
+// searchBtn.on("click", getCityName);
 cityInputEl.on("change", getCityName);
 
 function getCityName() {
-    cityInputVal = cityInputEl.val();
+    cityInputVal = cityInputEl.val().trim();
+    cityInputEl.val("");
+    localStorage.setItem("cityName", cityInputVal);
     console.log(cityInputVal);
     console.log(today);
     todaysForecastEl.children('h2').text(cityInputVal + " " + today);
+    var newCityEl = $('<button>');
+    newCityEl.addClass("btn btn-secondary");
+    newCityEl.attr("type", "submit");
+    newCityEl.text(cityInputVal);
+    cityListEl.append(newCityEl);
 }
