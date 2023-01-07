fd793365f9dd85017d226588e784c3b8bec8e1d8
mloeff <michaelloeffler23@gmail.com>
2023-01-07 17:20:07 -0600

    adds push functionality to add new cities to an array that is passed into localStorage with stringify, and retrieved from localStorage with parse

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,19 @@ var searchBtn = $('.btn'); // jquery selector for the
 var todaysForecastEl = $('#todaysForecast'); // jquery selector for the 
 //-- VARIABLE INITIALIZATIONS --//
 var cityInputVal = "";
+var cityNames = [];
+cityNames = JSON.parse(localStorage.getItem("cityList"));
 var today = dayjs().format('(M/D/YY)');
-var weatherAPI
+
 //-- EVENT LISTENTERS --//
 // searchBtn.on("click", getCityName);
 cityInputEl.on("change", getCityName);
 
 function getCityName() {
     cityInputVal = cityInputEl.val().trim();
+    cityNames.push(cityInputVal);
     cityInputEl.val("");
-    localStorage.setItem("cityName", cityInputVal);
-    console.log(cityInputVal);
-    console.log(today);
+    localStorage.setItem("cityList", JSON.stringify(cityNames));
     todaysForecastEl.children('h2').text(cityInputVal + " " + today);
     var newCityEl = $('<button>');
     newCityEl.addClass("btn btn-secondary");
