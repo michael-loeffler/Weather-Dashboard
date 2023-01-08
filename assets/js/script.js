a008ced2f2f4010b787bada15dbbf44f4c9cb8d1
mloeff <michaelloeffler23@gmail.com>
2023-01-08 01:23:30 -0600

    fixed fetch errors and formatted weather data for current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var cityInputEl = $('#citySearch'); // jquery selector for the
 var cityListEl = $('#cityList'); //jquery selector for the 
 var searchBtn = $('.btn'); // jquery selector for the 
 var todaysForecastEl = $('#todaysForecast'); // jquery selector for the 
+var todayTemp = $('#temp');
+var todayWind = $('#wind');
+var todayHumid = $('#humid'); 
 //-- VARIABLE INITIALIZATIONS --//
 var cityInputVal = "";
 var cityNames = [];
@@ -30,34 +33,44 @@ function getCityName() {
     newCityEl.attr("type", "submit");
     newCityEl.text(cityInputVal);
     cityListEl.append(newCityEl);
-    fetchWeather();
+    fetchGeo();
 }
 
-function fetchWeather() {
-    var geoAPI = "api.openweathermap.org/geo/1.0/direct?q=" + cityQuery;
+function fetchGeo() {
+    var geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityQuery + "&appid=3cd5ec4ae813460e4a92950deefd645a";
     fetch(geoAPI)
         .then(function (response) {
             if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    
-                    lat = data[0].lat;
-                    lon = data[0].lon;
-                    console.log(lat);
-                    weatherAPI = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon;
-                });
-            }
-        })
-   
 
-    fetch(weatherAPI)
-        .then(function (response) {
-            if (response.ok) {
-                console.log(response);
                 response.json().then(function (data) {
-                    console.log(data);
+                    fetchWeather(data);
 
                 });
             }
         })
 };
+
+function fetchWeather(geo) {
+    lat = JSON.stringify(geo[0].lat);
+    lon = JSON.stringify(geo[0].lon);
+    weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=3cd5ec4ae813460e4a92950deefd645a&units=imperial&cnt=30";
+    fetch(weatherAPI)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    formatWeather(data);
+                });
+            }
+        })
+};
+
+function formatWeather(data) {
+    var temp = data.list[2].main.temp;
+    var wind = data.list[2].wind.speed;
+    var humid = data.list[2].main.humidity;
+
+    todayTemp.text("Temp: " + temp + "°F");
+    todayWind.text("Wind: " + wind + " MPH");
+    todayHumid.text("Humidity: " + humid + " %");
+}
